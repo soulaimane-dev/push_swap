86a7a0e9e43016dcea6ecdea27a97f2c954daf8d
sbouabid <145467202+sbouabid1@users.noreply.github.com>
2024-01-13 17:50:13 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+GREEN="\033[92m"
+RED="\033[91m"
+
+from=-9999
+to=9999
+quantity=500
+
+while true
+do
+	numbers=$(seq $from $to | sort -R | head -n $quantity | paste -sd" " -)
+	ins=$(../push_swap/push_swap $numbers | wc -l | tr -d " ")
+	if [ $ins -ge 5500 ]
+	then
+		echo -e "${RED}$ins"
+		exit
+	else
+		echo -e "${GREEN}$ins"
+	fi
+	sleep 0.1
+done
